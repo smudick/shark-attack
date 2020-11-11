34f463125ef17f06a9287994a4102c5509e4ddc3
Sam Mudick <smudick@gmail.com>
2020-11-10 22:10:42 -0600

    cards rendering on DOM

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.scss';
+import '../styles/App.scss';
+import SharkTank from './components/SharkTank';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        <SharkTank />
       </div>
     );
   }
---
+++ src/App/components/LiveStudent/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+} from 'reactstrap';
+
+class LiveStudent extends React.Component {
+  render() {
+    const {
+      firstName, lastName, image,
+    } = this.props.student;
+    return (
+      <div className='live-student-cards col-3'>
+        <Card>
+          <CardImg top width='100%' size='sm' src={image} alt='Card image cap' />
+          <CardBody>
+            <CardTitle tag='h5'>
+              {firstName} {lastName}
+            </CardTitle>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default LiveStudent;
