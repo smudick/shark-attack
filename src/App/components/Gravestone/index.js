7c316a3c1b2422c232cc1c1907800368babb5f0c
Sam Mudick <smudick@gmail.com>
2020-11-11 19:01:01 -0600

    added graveyard and grave images and  updated layout

--- src/App/components/Gravestone/index.js
+++ src/App/components/Gravestone/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import {
-  Card,
-  CardImg,
-  CardBody,
-  CardTitle,
-} from 'reactstrap';
 
 class Gravestone extends React.Component {
   render() {
-    const {
-      firstName, lastName, image,
-    } = this.props.student;
+    const { firstName, lastName } = this.props.student;
     return (
-      <div className='dead-student-cards col-2 m-2'>
-        <Card>
-          <CardImg top width='100%' size='sm' src={image} alt='Card image cap' />
-          <CardBody>
-            <CardTitle tag='h5'>
-              {firstName} {lastName}
-              is dead
-            </CardTitle>
-          </CardBody>
-        </Card>
+      <div className='gravestone col-2 m-2'>
+        <h2 className='epitaph'>RIP <br></br>
+         {firstName} {lastName}</h2>
       </div>
     );
   }
